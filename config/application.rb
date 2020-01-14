f6d1d51b4d035339184c9dd75fa8d45ef04403a3
Alex <alext3hgr8@gmail.com>
2020-01-14 03:53:35 +0000

    Doing some more work trying to get hosting to work

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,16 @@ module RailsReactCalendar
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #logger = Logger.new(STDOUT)
+
+    config.before_configuration do
+        env_file = File.join(Rails.root, 'config', 'local_env.yml')
+        yaml = YAML.load(File.open(env_file))
+        #logger.debug(yaml)
+        
+        yaml.each do |key, value|
+          ENV[key.to_s] = value
+        end if File.exists?(env_file)
+    end
   end
 end
