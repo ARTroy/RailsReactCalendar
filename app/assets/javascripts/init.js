453bf2841b10e9c94e03c43b8a5f1112c60694eb
Alex <alext3hgr8@gmail.com>
2020-01-15 19:57:42 +0000

    added further fix for turbolinks

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,12 +1,6 @@
 ready = function(){
-    if ($('body').attr('data-loaded') == 'T'){
-        return;
-    }
     $(function(){ $(document).foundation(); });
-    $('body').attr('data-loaded','T');
 };
 
-$(document).ready(ready);
 $(document).on('turbolinks:load', ready);
-$(document).foundation();
-console.log("bruh");
+$(document).on("load", ready);
